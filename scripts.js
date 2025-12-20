eb110f51e9c9eab0e25453e381e92c476e9cd216
sai kaarthik <saikarthik.sk.2510@gmail.com>
2025-12-20 07:26:22 +0530

    Add scroll event listener to toggle 'scrolled' class

--- scripts.js
+++ scripts.js
@@ -204,3 +204,11 @@
   };
 
 })();
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    document.body.classList.add("scrolled");
+  } else {
+    document.body.classList.remove("scrolled");
+  }
+});
